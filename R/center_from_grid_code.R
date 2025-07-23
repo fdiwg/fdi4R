1a435dcf2b7ed61b849ff839f59e25f5d31ef741
ArturoMunozAlbero <101865073+ArturoMunozAlbero@users.noreply.github.com>
2025-07-23 13:34:55 +0200

    Migration of IOTC R libraries spatial-related functions
    Spatial-related functions included in the IOTC R libraries migrated to the fdi4R library.
    New functions copied from iotc_core_gis_cwp_utils (https://github.com/iotc-secretariat/iotc-lib-core-gis-cwp/blob/master/R/iotc_core_gis_cwp_utils.R):
    * center_from_grid_code
    * convert_CWP_grid
    * convert_to_CWP_grid
    * CWP_to_grid_coordinates

---
+++ R/center_from_grid_code.R
@@ -0,0 +1,44 @@
+#' Function copied from iotc_core_gis_cwp_utils (https://github.com/iotc-secretariat/iotc-lib-core-gis-cwp/blob/master/R/iotc_core_gis_cwp_utils.R)
+#' Returns details about the center of a regular grid provided the grid code.
+#' Details include:
+#'     the center latitude / longitude (regardless of the fraction of ocean area in the grid)
+#'     the grid width and height (in degrees)
+#'
+#' @param grid_code A grid code
+#' @return A named vector containing the center latitude (\code{y}) and longitude (\code{x}) plus the grid
+#' width (\code{size_lat}) and height (\code{size_lon}) in degrees
+#' @examples
+#' center_from_grid_code("5206066")
+#' center_from_grid_code("6205065")
+#' @export
+center_from_grid_code = function(grid_code) {
+  #Grid sizes by gridCode[1], i.e.:
+  #1 -> 30x30
+  #2 -> 10x20
+  #3 -> 10x10
+  #4 -> 20x20
+  #5 ->  1x1
+  #6 ->  5x5
+
+  #Height and width (in degrees) by type of grid (i.e. gridCode[0]) sorted by lexicographical first char code
+  #(1 = 30x30, 2 = 10x20, 3 = 10x10, 4 = 20x20, 5 = 1x1, 6 = 5x5)
+  sizes_lat = c(30, 10, 10, 20, 1, 5)
+  sizes_lon = c(30, 20, 10, 20, 1, 5)
+
+  grid_code = toString(grid_code)
+
+  size_code = as.numeric(substring(grid_code, 1, 1))
+
+  quadrant = as.numeric(substring(grid_code, 2, 2))
+
+  lat = as.numeric(substring(grid_code, 3, 4))
+  lon = as.numeric(substring(grid_code, 5, 7))
+
+  latitudes.quadrant  = c(  1, -1, -1,  1)
+  longitudes.quadrant = c(  1,  1, -1,  1)
+
+  lat = latitudes.quadrant[quadrant]  * ( lat + sizes_lat[size_code] / 2 );
+  lon = longitudes.quadrant[quadrant] * ( lon + sizes_lon[size_code] / 2 );
+
+  return (c(x = lon, y = lat, size_lat = sizes_lat[size_code], size_lon = sizes_lon[size_code]))
+}
---
+++ R/convert_to_CWP_grid.R
@@ -0,0 +1,35 @@
+#'Function copied from iotc_core_gis_cwp_utils (https://github.com/iotc-secretariat/iotc-lib-core-gis-cwp/blob/master/R/iotc_core_gis_cwp_utils.R)
+#'Converts a pair of decimal coordinate into a CWP grid code for a specific grid type
+#'@param lon Longitude (decimal coordinates)
+#'@param lat Latitude (decimal coordinates)
+#'@param grid_type_code The type of CWP grid (one among \code{\link{grid_1x1}}, \code{\link{grid_5x5}}, \code{\link{grid_10x10}}, \code{\link{grid_10x20}}, \code{\link{grid_20x20}} and \code{\link{grid_30x30}})
+#'@return The CWP grid code for the provided coordinates and grid type
+#'@export
+#'@examples convert_to_CWP_grid(20, -10, grid_1x1)
+convert_to_CWP_grid = function(lon, lat, grid_type_code = grid_5x5) {
+  q = NA
+
+  if     (lon >=0 && lat >=0) q = 1
+  else if(lon >=0 && lat < 0) q = 2
+  else if(lon < 0 && lat < 0) q = 3
+  else if(lon < 0 && lat >=0) q = 4
+
+  lat = floor(abs(lat))
+  lon = floor(abs(lon))
+
+  latS = 1
+  lonS = 1
+
+  grid = grid_char_1x1
+
+  if     (grid_type_code == grid_5x5)   { lonS = latS =  5;     grid = grid_char_5x5 }
+  else if(grid_type_code == grid_10x10) { lonS = latS = 10;     grid = grid_char_10x10 }
+  else if(grid_type_code == grid_20x20) { lonS = latS = 20;     grid = grid_char_20x20 }
+  else if(grid_type_code == grid_30x30) { lonS = latS = 30;     grid = grid_char_30x30 }
+  else if(grid_type_code == grid_10x20) { lonS = 20; latS = 10; grid = grid_char_10x20 }
+
+  lat = floor(lat / latS) * latS
+  lon = floor(lon / lonS) * lonS
+
+  return(paste0(grid, q, stri_pad(lat, 2, pad = "0"), stri_pad(lon, 3, pad = "0")))
+}
