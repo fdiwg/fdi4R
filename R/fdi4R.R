6a603ff503702bc1fd450d79d861d5dfc8063caa
eblondel <emmanuel.blondel1@gmail.com>
2025-09-11 10:04:53 +0200

    support #4

--- R/fdi4R.R
+++ R/fdi4R.R
@@ -2,8 +2,11 @@
 #'
 #' fdi4R provides a set of utility tools to do data processing in support of fisheries data interoperability operations
 #'
-#' @docType package
+#' @import sf
+#' @import geosphere
+#' @import parallel
+#'
 #' @name fdi4R
 #' @author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
 #' 
-NULL
+"_PACKAGE"
