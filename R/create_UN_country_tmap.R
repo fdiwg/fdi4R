227217757ee5291361f7fe502200948a1abbe874
eblondel <emmanuel.blondel1@gmail.com>
2026-01-14 09:33:57 +0100

    add create_UN_country_tmap widget function

---
+++ R/create_UN_country_tmap.R
@@ -0,0 +1,45 @@
+#' @name create_UN_country_tmap
+#' @title create_UN_country_tmap
+#' @description Creates a UN country tmap
+#' 
+#' @param iso3 country code
+#' @return an object of class \link[tmap]{tmap}
+#' 
+#' @author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
+#' @export
+#'
+create_UN_country_tmap <- function(iso3){
+  lines = fdi4R::un_boundaries
+  polygons = fdi4R::un_countries
+  
+  # Filter lines
+  filtered <- lines[lines$TYPE != 6 & (grepl(iso3, lines$ISO3_CNT1) | grepl(iso3, lines$ISO3_CNT2)), ]
+  
+  # Split by type
+  solid_lines <- filtered[!filtered$TYPE %in% c(3, 4, 9), ]
+  dashed_lines <- filtered[filtered$TYPE %in% c(2, 3, 4), ]
+  dotted_lines <- filtered[filtered$TYPE %in% c(8, 9), ]
+  
+  # polygon for selected country (light grey, no border)
+  poly_sel <- country_polys[country_polys$ISO_3 == iso3, ]
+  
+  # Start with empty map
+  map <- NULL
+  
+  # Add layers only if not empty
+  if (nrow(poly_sel) > 0) {
+    map <- map + tm_shape(poly_sel) +
+      tm_polygons(col = "grey", border.col = NA, alpha = 0.6)
+  }
+  if (nrow(solid_lines) > 0) {
+    map <- map + tm_shape(solid_lines) + tm_lines(lwd = 1.5, lty = "solid", col = "black")
+  }
+  if (nrow(dashed_lines) > 0) {
+    map <- map + tm_shape(dashed_lines) + tm_lines(lwd = 1.5, lty = "dashed", col = "black")
+  }
+  if (nrow(dotted_lines) > 0) {
+    map <- map + tm_shape(dotted_lines) + tm_lines(lwd = 1.5, lty = "dotted", col = "black")
+  }
+  
+  return(map)
+}
--- R/fdi4R.R
+++ R/fdi4R.R
@@ -1,11 +1,15 @@
 #' Tools for data processing in support of fisheries data interoperability operations
 #'
 #' fdi4R provides a set of utility tools to do data processing in support of fisheries data interoperability operations
-#'
-#' @import sf
-#' @import geosphere
-#' @import worrms
+#' 
 #' @import parallel
+#' @import tidyr
+#' @import sf
+#' @import lwgeom
+#' @import terra
+#' @import geosphere
+#' @import tmap
+#' @import worrms
 #'
 #' @name fdi4R
 #' @author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
